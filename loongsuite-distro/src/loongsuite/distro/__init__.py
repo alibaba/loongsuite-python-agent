b6437d3984a3ea83dcba1945b773f59f1a54ae30
cirilla-zmh <zmh1625@163.com>
2025-12-24 17:36:12 +0800

    Add loongsuite python agent distro
    Change-Id: Ibfdc9acd823041ade4ed9b4e75c37da0ded23b60
    Co-developed-by: Cursor <noreply@cursor.com>

---
+++ loongsuite-distro/src/loongsuite/distro/__init__.py
@@ -0,0 +1,47 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from typing import Any
+
+from opentelemetry.environment_variables import (
+    OTEL_LOGS_EXPORTER,
+    OTEL_METRICS_EXPORTER,
+    OTEL_TRACES_EXPORTER,
+)
+from opentelemetry.instrumentation.distro import BaseDistro
+from opentelemetry.sdk._configuration import _OTelSDKConfigurator
+from opentelemetry.sdk.environment_variables import OTEL_EXPORTER_OTLP_PROTOCOL
+
+
+class LoongSuiteConfigurator(_OTelSDKConfigurator):
+    """LoongSuite configurator, inherits from OpenTelemetry SDK configurator"""
+    pass
+
+
+class LoongSuiteDistro(BaseDistro):
+    """
+    LoongSuite Distro configures default OpenTelemetry settings.
+    
+    This is the Distro provided by LoongSuite, which configures default exporters and protocols.
+    """
+
+    # pylint: disable=no-self-use
+    def _configure(self, **kwargs: Any) -> None:
+        os.environ.setdefault(OTEL_TRACES_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_METRICS_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_LOGS_EXPORTER, "otlp")
+        os.environ.setdefault(OTEL_EXPORTER_OTLP_PROTOCOL, "grpc")
+
+
