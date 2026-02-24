open-telemetry/opentelemetry-python-contrib

Pass scope to get_sorted_metrics (#4150)
* Pass scope to get_sorted_metrics

* Lint

instrumentation/opentelemetry-instrumentation-pyramid/tests/test_automatic.py
@@ -92,6 +92,8 @@ _recommended_attrs = {
     MetricInstruments.HTTP_SERVER_DURATION: _server_duration_attrs_old,
 }
 
+SCOPE = "opentelemetry.instrumentation.pyramid.callbacks"
+
 
 class TestAutomatic(InstrumentationTest, WsgiTestBase):
     def setUp(self):
@@ -223,7 +225,7 @@ class TestAutomatic(InstrumentationTest, WsgiTestBase):
         self.client.get("/hello/756")
         self.client.get("/hello/756")
         self.client.get("/hello/756")
-        metrics = self.get_sorted_metrics()
+        metrics = self.get_sorted_metrics(SCOPE)
         number_data_point_seen = False
         histogram_data_point_seen = False
         self.assertEqual(len(metrics), 2)

alibaba/loongsuite-python-agent

Change-Id: Icca2e627b6eb61733682d873cd111ca9af0f4bfb
Co-developed-by: Cursor <noreply@cursor.com>

instrumentation/opentelemetry-instrumentation-pyramid/tests/test_automatic.py
@@ -49,6 +49,8 @@ _recommended_attrs = {
     "http.server.duration": _server_duration_attrs_old,
 }
 
+SCOPE = "opentelemetry.instrumentation.pyramid.callbacks"
+
 
 class TestAutomatic(InstrumentationTest, WsgiTestBase):
     def setUp(self):
@@ -180,7 +182,7 @@ class TestAutomatic(InstrumentationTest, WsgiTestBase):
         self.client.get("/hello/756")
         self.client.get("/hello/756")
         self.client.get("/hello/756")
-        metrics_list = self.memory_metrics_reader.get_metrics_data()
+        metrics_list = self.get_sorted_metrics(SCOPE)
         number_data_point_seen = False
         histogram_data_point_seen = False
         self.assertTrue(len(metrics_list.resource_metrics) == 1)