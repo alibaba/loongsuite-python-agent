4615d1ca67033de15d20917df14622dd3c41c616
流屿 <caishipeng.csp@alibaba-inc.com>
2026-09-10 13:54:21 +0800

    test(genai): repair CI fixtures and test dependencies

--- util/opentelemetry-util-genai/tests/test_http_suppression_integration.py
+++ util/opentelemetry-util-genai/tests/test_http_suppression_integration.py
@@ -1,14 +1,24 @@
+# Copyright The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import asyncio
 import threading
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
+import httpx
 import pytest
 
-pytest.importorskip("opentelemetry.instrumentation.httpx")
-
-import httpx
-
-from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
@@ -18,14 +28,17 @@ from opentelemetry.util.genai._suppression import suppress_http_instrumentation
 
 
 def test_http_plugin_recognizes_suppression():
+    httpx_instrumentation = pytest.importorskip(
+        "opentelemetry.instrumentation.httpx"
+    )
     exporter = InMemorySpanExporter()
     provider = TracerProvider()
     provider.add_span_processor(SimpleSpanProcessor(exporter))
-    plugin = HTTPXClientInstrumentor()
+    plugin = httpx_instrumentation.HTTPXClientInstrumentor()
     plugin.instrument(tracer_provider=provider)
 
     class Handler(BaseHTTPRequestHandler):
-        def do_GET(self):
+        def do_GET(self):  # pylint: disable=invalid-name
             self.send_response(200)
             self.end_headers()
 
